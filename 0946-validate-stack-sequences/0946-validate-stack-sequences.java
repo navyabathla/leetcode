ff1fb4671854e3545e8649cd35a01680499264c9
navyabathla <147905300+navyabathla@users.noreply.github.com>
2026-07-11 18:48:22 +0530

    Time: 1 ms (95.94%), Space: 46.2 MB (65.45%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        int i = 0;
+        int j = 0;
+        int n = pushed.length;
+
+        while (i < n && j < n) {
+
+            stack.push(pushed[i]);
+
+            while (!stack.isEmpty() && j < n && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+
+            i++;
+        }
+
+        return stack.isEmpty();
+    }
+}
