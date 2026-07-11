522d5534b020a86d563e329a067bce935efc9542
navyabathla <147905300+navyabathla@users.noreply.github.com>
2026-07-11 20:13:40 +0530

    Time: 4 ms (82.27%), Space: 45 MB (51.65%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            count[ch - 'a']++;
+        }
+
+        for (char ch : t.toCharArray()) {
+            count[ch - 'a']--;
+        }
+
+        // Check if all elements in the array are 0
+        boolean allZeros = Arrays.stream(count).allMatch(element -> element == 0);
+
+        return allZeros;
+    }
+}
