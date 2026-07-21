cb62adc9890c31ec7ea8c5d6b307e921a612f817
navyabathla <147905300+navyabathla@users.noreply.github.com>
2026-07-21 20:11:13 +0530

    Time: 4 ms (64.85%), Space: 45.3 MB (50.9%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+
+            int mask = 1 << i;
+            int countOne = 0;
+
+            for (int num : nums) {
+                if ((num & mask) != 0) {
+                    countOne++;
+                }
+            }
+
+            if (countOne % 3 == 1) {
+                result |= mask;
+            }
+        }
+
+        return result;
+    }
+}
