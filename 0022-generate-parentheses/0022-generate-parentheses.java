6d5556a0b6c73f46b9f8ea9111d77b48684e85cb
navyabathla <147905300+navyabathla@users.noreply.github.com>
2026-07-27 21:59:40 +0530

    Time: 0 ms (100%), Space: 44.5 MB (73.42%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+
+        backtrack(n, 0, 0, new StringBuilder(), result);
+
+        return result;
+    }
+
+    private void backtrack(int n, int open, int close,
+                           StringBuilder current,
+                           List<String> result) {
+
+        if (current.length() == 2 * n) {
+            result.add(current.toString());
+            return;
+        }
+
+        // Add '(' if we still have opening brackets left
+        if (open < n) {
+            current.append('(');
+            backtrack(n, open + 1, close, current, result);
+            current.deleteCharAt(current.length() - 1);
+        }
+
+        // Add ')' only if it won't make the string invalid
+        if (close < open) {
+            current.append(')');
+            backtrack(n, open, close + 1, current, result);
+            current.deleteCharAt(current.length() - 1);
+        }
+    }
+}
