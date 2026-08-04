5d83adf50bc031bae5616b67d3b2bf47db0bcdf4
navyabathla <147905300+navyabathla@users.noreply.github.com>
2026-08-04 13:42:14 +0530

    Time: 1 ms (100%), Space: 48.2 MB (23.51%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public int[] singleNumber(int[] nums) {
+
+        int xor = 0;
+
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        // Rightmost set bit
+        int diffBit = xor & (-xor);
+
+        int num1 = 0;
+        int num2 = 0;
+
+        for (int num : nums) {
+
+            if ((num & diffBit) == 0)
+                num1 ^= num;
+            else
+                num2 ^= num;
+        }
+
+        return new int[]{num1, num2};
+    }
+}
